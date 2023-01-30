da3120e093eac5d01d9887f4aec4570399ba60c1
Amil Silahic <jinobie02@gmail.com>
2023-01-30 15:48:11 -0700

    adding more styling to the top nav

--- components/Navigation/TopNav/TopNav.jsx
+++ components/Navigation/TopNav/TopNav.jsx
@@ -2,16 +2,17 @@ import { AiFillApple } from "react-icons/ai";
 
 const TopNav = ({}) => {
   return (
-    <div className="flex w-full h-6">
+    <div className="flex w-full h-8 items-center justify-center bg-transparent">
       <nav className="flex">
         <div className="flex gap-4">
-          <span className="text-lg ml-2 items-center flex text-white ">
+          <div className="text-lg h-full ml-2 items-center flex text-white ">
             <AiFillApple />
-          </span>
+          </div>
 
-          <span className="text-md flex items-center">Finder</span>
+          <p className="text-sm flex items-center">Finder</p>
         </div>
-        <div className="flex ml-4 items-center">
+
+        <div className="flex items-center ml-4 ">
           <ul className="text-sm flex gap-3 items-center">
             <li>File</li>
             <li>Edit</li>
@@ -19,13 +20,27 @@ const TopNav = ({}) => {
             <li>Go</li>
             <li>Window</li>
             <li>Help</li>
-            <div className="ml-auto flex items-center">
-              {/* need to make this based off the current date and time */}
-              <li>Mon Jan 30 03:33 PM</li>
-            </div>
           </ul>
         </div>
       </nav>
+      <div className="ml-auto flex items-center gap-4 text-sm">
+        <div>
+          <AiFillApple />
+        </div>
+        <div>
+          <AiFillApple />
+        </div>
+        <div>
+          <AiFillApple />
+        </div>
+        <div>
+          <AiFillApple />
+        </div>
+        <div className="flex items-center justify-center text-sm mr-4">
+          {/* need to make this based off the current date and time */}
+          <p>Mon Jan 30 03:33 PM</p>
+        </div>
+      </div>
     </div>
   );
 };

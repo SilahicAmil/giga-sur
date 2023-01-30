71ff165727cede1fdfa2344c1420a4127184c8d4
Amil Silahic <jinobie02@gmail.com>
2023-01-29 19:03:30 -0700

    giga sur init commit

---
+++ components/Navigation/TopNav.jsx
@@ -0,0 +1,19 @@
+const TopNav = ({}) => {
+  return (
+    <div className="flex w-full h-6">
+      <nav className="flex">
+        <div className="flex gap-4">
+          <span className="text-md ml-2">Logo</span>
+          <p>Finder</p>
+        </div>
+        <div className="flex ml-4">
+          <ul className="">
+            <li>Hello</li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default TopNav;
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import TopNav from "../components/Navigation/TopNav";
+
+function App() {
+  return (
+    <>
+      <TopNav />
+      <div>
+        <h1 className="text-sm bg-red-500 w-[100vw]">Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;

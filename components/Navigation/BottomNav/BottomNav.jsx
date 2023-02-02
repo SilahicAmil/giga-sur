7ebb7974939c3b29b5cfb39e3dfbed10de5d7133
Amil Silahic <jinobie02@gmail.com>
2023-02-01 23:01:07 -0700

    fixing photos for deployment

--- components/Navigation/BottomNav/BottomNav.jsx
+++ components/Navigation/BottomNav/BottomNav.jsx
@@ -1,6 +1,10 @@
 import BottomNavItems from "./BottomNavItems";
-import Finder from "../../../public/static/images/macos-finder-icon.jpg";
+import EmailIcon from "../../../public/static/images/email-icon.png";
+import FinderIcon from "../../../public/static/images/macos-finder-icon.jpg";
+import GithubIcon from "../../../public/static/images/github-logo.png";
 import ModalContent from "../../Modal/ModalContent";
+import SafariIcon from "../../../public/static/images/safari-icon.png";
+import SettingsIcon from "../../../public/static/images/settings-icon.png";
 import { createPortal } from "react-dom";
 import { useState } from "react";
 
@@ -11,37 +15,25 @@ const BottomNav = ({}) => {
   return (
     <div className="z-50 m-auto flex h-24 w-2/6 items-center justify-center gap-14 rounded-lg bg-slate-700 shadow-2xl">
       <BottomNavItems>
-        <img alt="finder image icon" src={Finder} className="rounded-lg" />
+        <img alt="finder image icon" src={FinderIcon} className="rounded-lg" />
       </BottomNavItems>
       {/* safari takes you to my portfolio */}
       <BottomNavItems>
         <a href="https://amil.dev" target="_blank">
-          <img
-            alt="safar icon"
-            src={"/public/static/images/safari-icon.png"}
-            className="rounded-lg"
-          />
+          <img alt="safar icon" src={SafariIcon} className="rounded-lg" />
         </a>
       </BottomNavItems>
       {/* Github goes to my github */}
       <BottomNavItems>
         <a href="https://github.com/silahicamil" target="_blank">
-          <img
-            alt="github icon"
-            src={"/public/static/images/github-logo.png"}
-            className="rounded-lg"
-          />
+          <img alt="github icon" src={GithubIcon} className="rounded-lg" />
         </a>
       </BottomNavItems>
       {/* this will pop up a modal to email me. will make it look similiar to mac os mail */}
       {/* Might want to make this into its own component later */}
       <BottomNavItems>
         <button onClick={(e) => setShowModal(true)}>
-          <img
-            alt="mac os email icon"
-            src={"/public/static/images/email-icon.png"}
-            className="rounded-lg"
-          />
+          <img alt="mac os email icon" src={EmailIcon} className="rounded-lg" />
         </button>
       </BottomNavItems>
       {showModal &&
@@ -57,7 +49,7 @@ const BottomNav = ({}) => {
         <p>
           <img
             alt="mac os settings icon"
-            src={"/public/static/images/settings-icon.png"}
+            src={SettingsIcon}
             className=" rounded-lg"
           />
         </p>

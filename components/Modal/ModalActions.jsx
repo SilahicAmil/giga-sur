2d7e0835db4be6cb19cb7748fd2246455a775b98
Amil Silahic <jinobie02@gmail.com>
2023-02-13 21:44:00 -0700

    redoing the email "app"

--- components/Modal/ModalActions.jsx
+++ components/Modal/ModalActions.jsx
@@ -1,17 +1,13 @@
+import { useState } from "react";
+
 const ModalActions = ({}) => {
   return (
     <>
       <div className="flex h-8 w-full items-center rounded-t-lg bg-[#1f2428]">
         <div className="ml-4 flex items-center gap-4">
-          <span className="flex h-4 w-4 items-center justify-center rounded-full bg-red-500 p-3">
-            X
-          </span>
-          <span className="flex h-4 w-4 items-center justify-center rounded-full bg-yellow-400 p-3">
-            -
-          </span>
-          <span className="flex h-4 w-4 items-center justify-center rounded-full bg-green-500 p-3">
-            +
-          </span>
+          <span className="flex h-4 w-4 items-center justify-center rounded-full bg-red-500 p-2"></span>
+          <span className="flex h-4 w-4 items-center justify-center rounded-full bg-yellow-400 p-2"></span>
+          <span className="flex h-4 w-4 items-center justify-center rounded-full bg-green-500 p-2"></span>
         </div>
       </div>
     </>
--- components/Modal/ModalContent.jsx
+++ components/Modal/ModalContent.jsx
@@ -10,39 +10,67 @@ const ModalContent = ({ children }) => {
     <>
       <ModalCard>
         <ModalActions />
-        <form
-          className="flex h-full  w-full flex-col items-center justify-center gap-8 rounded-lg bg-slate-400"
-          onSubmit={formSubmitHandler}
-        >
-          <div className="flex w-1/4 flex-col  ">
-            <label htmlFor="message_to">To:</label>
-            <input
-              type="text"
-              readOnly
-              value="Amil Silahic - amil@amil.dev"
-              className="flex w-full bg-white text-black"
-            />
+        <section class="h-full w-full bg-white dark:bg-gray-900">
+          <div class="mx-auto max-w-screen-md py-8 px-4 lg:py-16">
+            <form action="#" class="space-y-8">
+              <div>
+                <label
+                  for="email"
+                  class="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  class="focus:ring-primary-500 focus:border-primary-500 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
+                  placeholder="name@gmail.com"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="subject"
+                  class="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Subject
+                </label>
+                <input
+                  type="text"
+                  id="subject"
+                  class="focus:ring-primary-500 focus:border-primary-500 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light block w-full rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
+                  placeholder="Let me know how you feel!"
+                  required
+                />
+              </div>
+              <div class="sm:col-span-2">
+                <label
+                  for="message"
+                  class="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-400"
+                >
+                  Your message
+                </label>
+                <textarea
+                  id="message"
+                  rows="6"
+                  class="focus:ring-primary-500 focus:border-primary-500 dark:focus:ring-primary-500 dark:focus:border-primary-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
+                  placeholder="Leave a message..."
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                class="hover:bg-primary-800  dark:hover:bg-primary-700 rounded-lg bg-sky-700 py-3 px-5  text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-sky-300 dark:bg-sky-600 dark:focus:ring-sky-800 sm:w-fit"
+              >
+                Send message
+              </button>
+            </form>
           </div>
-
-          <div className="flex w-1/4 flex-col">
-            <label htmlFor="subject">Subject:</label>
-            <input type="text" name="subject" className="bg-white text-black" />
+          <div className="text-lg">
+            <button className="ml-auto flex w-full items-center justify-center rounded-sm bg-sky-500 p-1">
+              {children}
+            </button>
           </div>
-
-          <div className="flex flex-col">
-            <label htmlFor="message">Message:</label>
-            <textarea
-              name="message"
-              cols="40"
-              rows="5"
-              className="bg-white text-black"
-            />
-          </div>
-          <div>
-            <button type="submit">Send</button>
-          </div>
-          {children}
-        </form>
+        </section>
       </ModalCard>
     </>
   );

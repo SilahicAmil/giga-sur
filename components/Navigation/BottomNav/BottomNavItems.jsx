2203d0f8b2890d02772af5f4251b1b3f9e479917
Amil Silahic <jinobie02@gmail.com>
2023-02-13 22:28:48 -0700

    adding more styling

--- components/Navigation/BottomNav/BottomNavItems.jsx
+++ components/Navigation/BottomNav/BottomNavItems.jsx
@@ -1,6 +1,6 @@
 const BottomNavItems = ({ children }) => {
   return (
-    <div className=" flex h-16 w-16 transform items-center rounded-lg bg-white duration-150 hover:scale-150 hover:cursor-pointer">
+    <div className="flex h-16 w-16 transform items-center rounded-lg bg-white duration-150 hover:scale-150 hover:cursor-pointer">
       {children}
     </div>
   );

2e885b063f5cabd9d1f66f8b543069c867ef690d
Amil Silahic <jinobie02@gmail.com>
2023-02-13 22:32:43 -0700

    making buttons bigger for modal actions

--- components/ModalForm/ModalActions.jsx
+++ components/ModalForm/ModalActions.jsx
@@ -3,13 +3,13 @@ const ModalActions = ({ children }) => {
     <>
       <div className="flex h-8 w-full items-center rounded-t-lg bg-[#1f2428]">
         <div className="ml-4 flex items-center gap-4">
-          <span className="flex h-4 w-4 items-center justify-center rounded-full bg-red-500 p-2">
+          <span className="flex h-6 w-6 items-center justify-center rounded-full bg-red-500 p-2">
             {children}
           </span>
-          <span className="flex h-4 w-4 items-center justify-center rounded-full bg-yellow-400 p-2">
+          <span className="flex h-6 w-6 items-center justify-center rounded-full bg-yellow-400 p-2">
             -
           </span>
-          <span className="flex h-4 w-4 items-center justify-center rounded-full bg-green-500 p-2">
+          <span className="flex h-6 w-6 items-center justify-center rounded-full bg-green-500 p-2">
             +
           </span>
         </div>

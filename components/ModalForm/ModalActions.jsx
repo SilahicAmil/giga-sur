e02fbe11e1d654cda5d2ef3c03c51d9d32fc03eb
Amil Silahic <jinobie02@gmail.com>
2023-03-15 12:00:37 -0600

    adding toast for sending message

--- components/ModalForm/ModalActions.jsx
+++ components/ModalForm/ModalActions.jsx
@@ -2,8 +2,8 @@ const ModalActions = ({ children }) => {
   return (
     <>
       <div className="flex h-10 w-full items-center rounded-t-lg bg-[#1f2428]">
-        <div className="ml-4 flex items-center gap-4">
-          <span className="flex h-6 w-6 items-center justify-center rounded-full bg-red-500 p-2 text-white">
+        <div className="ml-4 flex h-10 items-center gap-4">
+          <span className="flex h-8 w-8 items-center justify-center rounded-full bg-red-500 p-2 text-xl text-white">
             {children}
           </span>
         </div>
--- components/ModalForm/ModalContent.jsx
+++ components/ModalForm/ModalContent.jsx
@@ -1,3 +1,4 @@
+import toast, { Toaster } from "react-hot-toast";
 import { useRef, useState } from "react";
 
 import ModalActions from "../ModalForm/ModalActions";
@@ -6,8 +7,6 @@ import emailjs from "@emailjs/browser";
 
 const ModalContent = ({ children }) => {
   const formRef = useRef();
-
-  const [formSubmitted, setFormSubmitted] = useState(false);
 
   console.log(import.meta.env.VITE_EMAIL_PUBLIC_KEY);
 
@@ -21,17 +20,22 @@ const ModalContent = ({ children }) => {
       import.meta.env.VITE_EMAIL_PUBLIC_KEY
     );
 
-    setFormSubmitted(true);
     formRef.current.reset();
+    return toast.success("Message Sent!");
   };
 
   return (
     <>
+      <Toaster
+        toastOptions={{
+          duration: 5000,
+        }}
+      />
       <ModalCard>
         <ModalActions>{children}</ModalActions>
 
         <section className="h-full w-full bg-white dark:bg-gray-900">
-          <div className="mx-auto max-w-screen-md py-8 px-4 lg:py-16">
+          <div className="mx-auto max-w-screen-md py-8 px-4 lg:py-8">
             <form
               onSubmit={formSubmitHandler}
               ref={formRef}
@@ -69,7 +73,21 @@ const ModalContent = ({ children }) => {
                   required
                 />
               </div>
-
+              <div>
+                <label
+                  htmlFor="myemail"
+                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Send To
+                </label>
+                <input
+                  type="myemail"
+                  id="myemail"
+                  className="focus:ring-primary-500 focus:border-primary-500 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
+                  placeholder="amil@amil.dev"
+                  readOnly
+                />
+              </div>
               <div className="sm:col-span-2">
                 <label
                   htmlFor="message"
@@ -87,21 +105,13 @@ const ModalContent = ({ children }) => {
               </div>
               <button
                 type="submit"
-                className="hover:bg-primary-800  dark:hover:bg-primary-700 rounded-lg bg-sky-700 py-3 px-5  text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-sky-300 dark:bg-sky-600 dark:focus:ring-sky-800 sm:w-fit"
+                className="hover:bg-primary-800 dark:hover:bg-primary-700  rounded-lg bg-sky-700 py-3 px-5  text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-sky-300 dark:bg-sky-600 dark:focus:ring-sky-800 sm:w-fit"
               >
                 Send message
               </button>
             </form>
-            {formSubmitted ? (
-              <div>
-                <p className="text-white">Message Sent!</p>
-              </div>
-            ) : undefined}
           </div>
         </section>
-        {/* <div className=" flex h-8 w-full items-center justify-center rounded-sm bg-sky-300 text-lg text-black">
-          {children}
-        </div> */}
       </ModalCard>
     </>
   );

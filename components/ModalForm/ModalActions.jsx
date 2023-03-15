d5af2edf68d1e1f7bc8f13882055ba12bbd644f1
Amil Silahic <jinobie02@gmail.com>
2023-03-15 11:35:43 -0600

    feat: :art: Adding Faux Safari search bar to SafariModal and updating Modal Actions to only have X button

--- components/ModalForm/ModalActions.jsx
+++ components/ModalForm/ModalActions.jsx
@@ -1,16 +1,10 @@
 const ModalActions = ({ children }) => {
   return (
     <>
-      <div className="flex h-8 w-full items-center rounded-t-lg bg-[#1f2428]">
+      <div className="flex h-10 w-full items-center rounded-t-lg bg-[#1f2428]">
         <div className="ml-4 flex items-center gap-4">
-          <span className="flex h-6 w-6 items-center justify-center rounded-full bg-red-500 p-2">
+          <span className="flex h-6 w-6 items-center justify-center rounded-full bg-red-500 p-2 text-white">
             {children}
-          </span>
-          <span className="flex h-6 w-6 items-center justify-center rounded-full bg-yellow-400 p-2">
-            -
-          </span>
-          <span className="flex h-6 w-6 items-center justify-center rounded-full bg-green-500 p-2">
-            +
           </span>
         </div>
       </div>
--- components/WebModal/WebModalContent.jsx
+++ components/WebModal/WebModalContent.jsx
@@ -1,15 +1,25 @@
-import Draggable, { DraggableCore } from "react-draggable";
-
-import ModalCard from "../ModalForm/ModalCard";
+import { AiOutlineLock } from "react-icons/ai";
+import Draggable from "react-draggable";
 
 const WebModalContent = ({ children }) => {
   return (
     <Draggable>
       <div className="absolute top-0 left-0 bottom-0 right-0 z-50 m-10 ml-auto h-3/4 w-4/6 items-center justify-center rounded-lg  bg-[#1f2428] shadow-2xl">
         <div className=" mt-2 h-full w-full ">
-          <button className="mr-auto ml-4 flex h-6  w-6 items-center justify-center rounded-full bg-red-500 p-2  text-white">
-            {children}
-          </button>
+          <div className="mr-auto ml-4 flex h-8  w-full items-center">
+            <button className="flex h-6 w-6 items-center justify-center rounded-full bg-red-500 p-2  text-white">
+              {children}
+            </button>
+
+            <div className="flex w-full items-center justify-center">
+              <div className="text-md flex w-1/3 justify-center rounded-md border border-gray-500">
+                <p className="flex items-center gap-2 text-white">
+                  <AiOutlineLock />
+                  https://amil.dev
+                </p>
+              </div>
+            </div>
+          </div>
 
           <div className=" flex h-full w-full flex-col ">
             <iframe
